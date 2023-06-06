db0073f61eaa9d24a6774e7968ce637f2b3afbfc
brunocoronado49 <francorangelcoronado@gmail.com>
2023-06-06 00:21:43 -0500

    my first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte edad = 25;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Francisco Rangel");
+            Console.WriteLine($"Mi edad es de {edad}");
+        }
+    }
+}
